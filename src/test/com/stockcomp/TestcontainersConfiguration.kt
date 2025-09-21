5b02e57d7a2c580f7bda802bdebd62f081d1887c
Andreas Wassum <a.wassum@gmail.com>
2025-09-21 18:17:27 +0200

    Changed configuration for testcontainers

---
+++ src/test/com/stockcomp/TestcontainersConfiguration.kt
@@ -0,0 +1,20 @@
+package com.stockcomp
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
+
+
+@TestConfiguration
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    fun postgresContainer() = postgresContainer
+
+    companion object {
+        private val postgresContainer = PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:16.3-bullseye"))
+    }
+}
---
+++ src/test/com/stockcomp/producer/PlaceholderIntegrationIT.kt
@@ -0,0 +1,15 @@
+package com.stockcomp.producer
+
+import com.stockcomp.TestcontainersConfiguration
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@SpringBootTest
+@Import(TestcontainersConfiguration::class)
+internal class PlaceholderIntegrationIT {
+
+    @Test
+    fun `application context test`() {
+    }
+}
